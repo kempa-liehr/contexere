327991e77bc01451d784253151b11bac3ca663d2
Andreas Kempa-Liehr <kempa-liehr@protonmail.com>
2024-10-31 08:48:08 +1300

    Update docstring

--- src/contexere/name.py
+++ src/contexere/name.py
@@ -97,6 +97,6 @@ if __name__ == "__main__":
     # After installing your project with pip, users can also run your Python
     # modules as scripts via the ``-m`` flag, as defined in PEP 338::
     #
-    #     python -m contexere.skeleton 42
+    #     python -m contexere.name
     #
     run()
