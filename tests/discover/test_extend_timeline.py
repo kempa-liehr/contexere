28105c7b7035b1895199f75e45968fe7609968ff
Andreas W. Kempa-Liehr <a.kempa-liehr@auckland.ac.nz>
2026-02-04 15:26:35 +1300

    Cloned from test_grow_context.py

---
+++ tests/discover/test_extend_timeline.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from contexere.discover import grow_context
+
+def test_grow_context_empty_dict():
+    context = dict()
+    grow_context(context, 'test', '26p4', 'a', Path.home())
+    assert context['test']['26p4']['a'] == [Path.home()]
+
+    grow_context(context, 'test', '26p4', 'a', Path.home())
+    assert context['test']['26p4']['a'] == [Path.home(), Path.home()]
+
+    grow_context(context, 'test', '26p4', 'b', Path.home())
+    assert context['test']['26p4']['b'] == [Path.home()]
+
+
