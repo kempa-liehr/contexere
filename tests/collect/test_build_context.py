f862ddc7ff1473f2a532a051abb7f4271308d1ef
Andreas W. Kempa-Liehr <a.kempa-liehr@auckland.ac.nz>
2026-02-11 11:01:41 +1300

    Enable optional recursive file collection while building the context.

--- tests/collect/test_build_context.py
+++ tests/collect/test_build_context.py
@@ -64,3 +64,64 @@ def test_three_related_files_with_project_filter(temp_dir):
     assert set(timeline['26pA']['ERP']['a']) == set([temp_dir / 'ERP26pAa_data.csv',
                                                     temp_dir / 'ERP26pAa_example.txt'])
     assert '26pB' not in timeline
+
+def test_three_related_files_different_projects_and_folder_non_recursive(temp_dir):
+    fill_folder(temp_dir, 'ERP26pAa_example.txt', 'ERQ26pBa_example.txt', 'ERP26pAa_data.csv')
+    sub_dir = temp_dir / 'sub_dir'
+    sub_dir.mkdir()
+    fill_folder(sub_dir, 'ERP26pAa_example.png')
+    context, timeline = build_context(temp_dir, recursive=False)
+    assert set(context['ERP']['26pA']['a']) == set([temp_dir / 'ERP26pAa_data.csv',
+                                                   temp_dir / 'ERP26pAa_example.txt'])
+    assert context['ERQ']['26pB']['a'] == [temp_dir / 'ERQ26pBa_example.txt']
+    assert set(timeline['26pA']['ERP']['a']) == set([temp_dir / 'ERP26pAa_data.csv',
+                                                    temp_dir / 'ERP26pAa_example.txt'])
+    assert timeline['26pB'] == {'ERQ': {'a': [temp_dir / 'ERQ26pBa_example.txt']}}
+
+def test_three_related_files_different_projects_and_folder_recursive(temp_dir):
+    fill_folder(temp_dir, 'ERP26pAa_example.txt', 'ERQ26pBa_example.txt', 'ERP26pAa_data.csv')
+    sub_dir = temp_dir / 'sub_dir'
+    sub_dir.mkdir()
+    fill_folder(sub_dir, 'ERP26pAa_example.png')
+    context, timeline = build_context(temp_dir, recursive=True)
+    assert set(context['ERP']['26pA']['a']) == set([temp_dir / 'ERP26pAa_data.csv',
+                                                   temp_dir / 'ERP26pAa_example.txt',
+                                                    sub_dir / 'ERP26pAa_example.png'
+                                                    ])
+    assert context['ERQ']['26pB']['a'] == [temp_dir / 'ERQ26pBa_example.txt']
+    assert set(timeline['26pA']['ERP']['a']) == set([temp_dir / 'ERP26pAa_data.csv',
+                                                    temp_dir / 'ERP26pAa_example.txt',
+                                                     sub_dir / 'ERP26pAa_example.png'])
+    assert timeline['26pB'] == {'ERQ': {'a': [temp_dir / 'ERQ26pBa_example.txt']}}
+
+def test_three_related_files_different_projects__recursive_True__project_filter_ER(temp_dir):
+    fill_folder(temp_dir, 'ERP26pAa_example.txt', 'ERQ26pBa_example.txt', 'ERP26pAa_data.csv')
+    sub_dir = temp_dir / 'sub_dir'
+    sub_dir.mkdir()
+    fill_folder(sub_dir, 'ERP26pAa_example.png')
+    context, timeline = build_context(temp_dir, recursive=True, project_filter='ER')
+    assert set(context['ERP']['26pA']['a']) == set([temp_dir / 'ERP26pAa_data.csv',
+                                                   temp_dir / 'ERP26pAa_example.txt',
+                                                    sub_dir / 'ERP26pAa_example.png'
+                                                    ])
+    assert context['ERQ']['26pB']['a'] == [temp_dir / 'ERQ26pBa_example.txt']
+    assert set(timeline['26pA']['ERP']['a']) == set([temp_dir / 'ERP26pAa_data.csv',
+                                                    temp_dir / 'ERP26pAa_example.txt',
+                                                     sub_dir / 'ERP26pAa_example.png'])
+    assert timeline['26pB'] == {'ERQ': {'a': [temp_dir / 'ERQ26pBa_example.txt']}}
+
+def test_three_related_files_different_projects__recursive_True__project_filter_ERP(temp_dir):
+    fill_folder(temp_dir, 'ERP26pAa_example.txt', 'ERQ26pBa_example.txt', 'ERP26pAa_data.csv')
+    sub_dir = temp_dir / 'sub_dir'
+    sub_dir.mkdir()
+    fill_folder(sub_dir, 'ERP26pAa_example.png')
+    context, timeline = build_context(temp_dir, recursive=True, project_filter='ERP')
+    assert set(context['ERP']['26pA']['a']) == set([temp_dir / 'ERP26pAa_data.csv',
+                                                   temp_dir / 'ERP26pAa_example.txt',
+                                                    sub_dir / 'ERP26pAa_example.png'
+                                                    ])
+    assert set(timeline['26pA']['ERP']['a']) == set([temp_dir / 'ERP26pAa_data.csv',
+                                                    temp_dir / 'ERP26pAa_example.txt',
+                                                     sub_dir / 'ERP26pAa_example.png'])
+    assert 'ERQ' not in context
+    assert '26pB' not in timeline
