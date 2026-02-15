ba06e4061e5e30be501ff0b0196ee2c6124e9fff
Andreas W. Kempa-Liehr <a.kempa-liehr@auckland.ac.nz>
2026-02-16 08:03:21 +1300

    Init Researcher table and read table as pandas DataFrame.
    .read() adapted from DS26pGa

---
+++ src/contexere/data/cache.py
@@ -0,0 +1,29 @@
+import os
+from sqlalchemy import inspect, insert
+
+from contexere import conf
+import contexere.data.interfaces.contextdb as cdb
+
+def init_cache(path, db):
+    fill_cache(path, db)
+    return db
+
+def init_user(db, user=conf.username):
+    if len(db.get_researchers()) == 0:
+        stmt = insert(cdb.researcher).values(Name=user)
+        db.connect()
+        db.connection.execute(stmt)
+        result = db.connection.commit()
+    else:
+        result = None
+    return result
+
+def fill_cache(db, path='/'):
+    db.create_tables()
+    init_user(db)
+
+if __name__ == '__main__':
+    db = cdb.ContextDB(path='')
+    db.create_tables()
+    result = init_user(db)
+    print(db.get_researchers())
