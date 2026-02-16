ed5c05a50dd579f221c90d3327030127fec58fcd
Andreas W. Kempa-Liehr <a.kempa-liehr@auckland.ac.nz>
2026-02-16 15:01:45 +1300

    The scope of the test case for function index_file_artefact() fits more to function fill_cache().

---
+++ tests/data/cache/test_fill_cache.py
@@ -0,0 +1,78 @@
+import pytest
+from contexere.data.cache import init_researcher_table
+from contexere.data.context import index_file_artefact
+from contexere.data.interfaces.contextdb import ContextDB
+
+@pytest.fixture()
+def temp_dir(tmp_path_factory):
+    # Create a named base directory for the session
+    base = tmp_path_factory.mktemp("session_data")
+    return base
+
+@pytest.fixture
+def db():
+    in_memory_db = ContextDB(path='')
+    in_memory_db.create_tables()
+    return in_memory_db
+
+def fill_folder(path, *files):
+    for fn in files:
+        (path / fn).write_text("x", encoding="utf-8")
+
+def test_unrelated_and_related_file(temp_dir, db):
+    init_researcher_table(db, user='testuser')
+    fill_folder(temp_dir, 'notes.txt', 'ERP26pBa_9b__example__value_1.txt')
+    with open(temp_dir / 'logbook.org', 'w') as f:
+        f.write('Title\n* ERP26p9b -- Extracted note\n')
+    index_file_artefact(db, temp_dir, 'ERP', '26pB', 'a', 'example.txt')
+    tables = {table: db.select_all(table) for table in db.metadata.tables}
+    print(tables)
+    for table, df in tables.items():
+        print(table)
+        print(df)
+        if table in ['RAG', 'Keyword', 'KeywordIndex']:
+            assert len(df) == 2
+        else:
+            assert len(df) == 1
+        if table == 'RAG':
+            assert table['RAG'].loc[1, 'ID'] == 'ERP26p9b'
+        else:
+            assert table.index[0] == 1
+    assert tables['Researcher'].loc[0, 'Name'] == 'testuser'
+    assert tables['Project'].loc[0, 'Name'] == 'ERP'
+    assert tables['RAG'].loc[0, 'Project'] == 1
+    assert tables['RAG'].loc[0, 'Date'] == '26pB'
+    assert tables['RAG'].loc[0, 'Enumerator'] == 'a'
+    assert tables['RAG'].loc[0, 'ResearcherID'] == '1'
+    assert tables['RAG'].loc[1, 'Project'] == 1
+    assert tables['RAG'].loc[1, 'Date'] == '26p9'
+    assert tables['RAG'].loc[1, 'Enumerator'] == 'b'
+    assert tables['RAG'].loc[1, 'ResearcherID'] == '1'
+    assert tables['KnowledeGraph'].loc[0, 'Parent'] == 'ERP26p9b'
+    assert tables['KnowledeGraph'].loc[0, 'Child'] == 'ERP26pBa'
+    assert tables['Path'].loc[0, 'Path'] == str(temp_dir)
+    assert tables['Artefact'].loc[0, 'RAG'] == 'ERP26p9b'
+    assert tables['Artefact'].loc[0, 'FileName'] == 'ERP26pBa_9b__example__value_1.txt'
+    assert tables['Artefact'].loc[0, 'FileExtension'] == 'txt'
+    assert tables['Artefact'].loc[0, 'Path'] == 1
+    assert not tables['Artefact'].loc[0, 'IsGenerator']
+    assert tables['MarkupFile'].loc[0, 'Filename'] == 'logbook.org'
+    assert tables['MarkupFile'].loc[0, 'FileExtension'] == 'org'
+    assert tables['MarkupFile'].loc[0, 'Path'] == 1
+    assert tables['Note'].loc[0, 'RAG'] == 'ERP26p9b'
+    assert tables['Note'].loc[0, 'File'] == 1
+    assert tables['Note'].loc[0, 'Quote'] == '* ERP26p9b -- Extracted note'
+    assert tables['Note'].loc[0, 'LineNr'] == 1
+    assert tables['Keyword'].loc[0, 'Keyword'] == 'example'
+    assert tables['Keyword'].loc[1, 'Keyword'] == 'value'
+    assert tables['KeywordIndex'].loc[0, 'RAG'] == 'ERP26pBa'
+    assert tables['KeywordIndex'].loc[0, 'Keyword'] == 1
+    assert tables['KeywordIndex'].loc[1, 'RAG'] == 'ERP26pBa'
+    assert tables['KeywordIndex'].loc[1, 'Keyword'] == 2
+    assert tables['KeyValueIndex'].loc[0, 'RAG'] == 'ERP26pBa'
+    assert tables['KeyValueIndex'].loc[0, 'Keyword'] == 2
+    assert tables['KeyValueIndex'].loc[0, 'Value'] == '1'
+    assert tables['KeyValueIndex'].loc[0, 'IsNumeric']
+
+
+
