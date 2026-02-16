beaed04b3dc1e11f9d623782024eb1d3447f5837
Andreas W. Kempa-Liehr <a.kempa-liehr@auckland.ac.nz>
2026-02-16 13:30:42 +1300

    Extended regular expression to match keywords
    Adapted from DS26pGe, f, g

--- src/contexere/data/context.py
+++ src/contexere/data/context.py
@@ -1,5 +1,15 @@
 import re
 
 # Define the scheme with named groups
-__pattern__ = re.compile(r'(?P<project>[a-zA-Z]*)(?P<date>[0-9]{2}[o-z][1-9A-V])(?P<step>[a-z]*)_?')
+__pattern__ = re.compile(r'^(?P<project>[a-zA-Z]+)(?P<date>[0-9]{2}[o-z][1-9A-V])(?P<step>[a-z]+)(?:_+(?P<kwds>.+))?_*')
 
+def confirm_rag(token, pattern=__pattern__):
+    match = pattern.match(token)
+    if match:
+        project = match.group('project')
+        date = match.group('date')
+        step = match.group('step')
+        keywords = match.group('kwds')
+    else:
+        project, date, step, keywords = None, None, None, None
+    return match, project, date, step, keywords
