41cd1e965690bf4a4e5eb3b276f15092d5de72bf
Andreas W. Kempa-Liehr <a.kempa-liehr@auckland.ac.nz>
2026-02-18 10:34:21 +1300

    Provide list of files and folders to be excluded from catalog.

--- src/contexere/data/cache.py
+++ src/contexere/data/cache.py
@@ -1,16 +1,21 @@
-
+from contexere.conf import __IGNORE__
 from contexere.data.context import confirm_rag, index_file_artefact
 import contexere.data.interfaces.contextdb as cdb
+
+def exclude_path(path, ignore=__IGNORE__):
+    exclude = any([path.match(pattern) for pattern in ignore])
+    return exclude
 
 def fill_cache(db, root='/'):
     db.create_tables()
     for path in root.rglob('*'):
-        match, project, date, step, keywords = confirm_rag(path.name)
-        if match:
-            try:
-                index_file_artefact(db, path, project, date, step, keywords)
-            except:
-                print(path)
+        if not exclude_path(path):
+            match, project, date, step, keywords = confirm_rag(path.name)
+            if match:
+                try:
+                    index_file_artefact(db, path, project, date, step, keywords)
+                except:
+                    print(path)
 
 
 if __name__ == '__main__':
