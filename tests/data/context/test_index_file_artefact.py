c7b24a1fde23df4153b23a4708e805e187b208e1
Andreas W. Kempa-Liehr <a.kempa-liehr@auckland.ac.nz>
2026-02-18 11:29:15 +1300

    Capture artefact filenames without suffix.

--- tests/data/context/test_index_file_artefact.py
+++ tests/data/context/test_index_file_artefact.py
@@ -53,5 +53,34 @@ def test_file_with_dependency(temp_dir, db):
     assert tables['KeyValueIndex'].loc[0, 'Value'] == '1'
     assert tables['KeyValueIndex'].loc[0, 'IsNumeric']
 
+def test_folder(temp_dir, db):
+    rag_dir = temp_dir / 'ERP26pBa_test'
+    rag_dir.mkdir()
+    index_file_artefact(db, rag_dir,
+                        'ERP', '26pB', 'a', 'test')
+    tables = {table: db.select_all(table) for table in db.metadata.tables}
+    for table, df in tables.items():
+        print(table)
+        if table in ['KnowledgeGraph', 'KeyValueIndex']:
+            assert len(df) == 0
+        else:
+            assert len(df) == 1
+        if table == 'RAG':
+            assert df.loc[0, 'ID'] == 'ERP26pBa'
+    assert tables['Project'].loc[0, 'Name'] == 'ERP'
+    assert tables['RAG'].loc[0, 'Project'] == 'ERP'
+    assert tables['RAG'].loc[0, 'Date'] == '26pB'
+    assert tables['RAG'].loc[0, 'Step'] == 'a'
+    assert tables['Path'].loc[0, 'Name'] == str(temp_dir)
+    assert tables['Artefact'].loc[0, 'RAG'] == 'ERP26pBa'
+    assert tables['Artefact'].loc[0, 'FileName'] == 'ERP26pBa_test'
+    assert tables['Artefact'].loc[0, 'FileExtension'] == ''
+    assert tables['Artefact'].loc[0, 'Path'] == 1
+    assert not tables['Artefact'].loc[0, 'IsGenerator']
+    assert tables['Artefact'].loc[0, 'IsDirectory']
+    assert tables['Keyword'].loc[0, 'Keyword'] == 'test'
+    assert tables['KeywordIndex'].loc[0, 'RAG'] == 'ERP26pBa'
+    assert tables['KeywordIndex'].loc[0, 'Keyword'] == 1
+
 
 
