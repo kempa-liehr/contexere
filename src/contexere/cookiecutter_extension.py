df71dce1c16d41cdce86a85c0b098d3ac2ba1e5b
Andreas W. Kempa-Liehr <kempa-liehr@protonmail.com>
2026-03-07 15:09:24 +1300

    Extend the jinja2 environment to support initial entries for the project logbook.

--- src/contexere/cookiecutter_extension.py
+++ src/contexere/cookiecutter_extension.py
@@ -1,13 +1,30 @@
-# ccds_extensions.py
+import datetime
 from jinja2 import Environment
 from jinja2.ext import Extension
+from tzlocal import get_localzone
 
+from contexere.scheme import abbreviate_date
 
 def _acronym(s: str) -> str:
     """Return the first letter of each word after replacing dashes and underscores with spaces."""
     return ''.join([word[0] for word in
                     s.replace('-', ' ').replace('_', ' ' ).split()])
 
+def _abbrmonth() -> str:
+    """Return the abbreviated year-month according to the contexere scheme."""
+    return abbreviate_date(local=True)[:-1]
+
+def _abbrdate() -> str:
+    """Return the abbreviated date according to the contexere scheme."""
+    return abbreviate_date(local=True)
+
+def _month():
+    """Return the month and year of the current date."""
+    return datetime.datetime.now(tz=get_localzone()).strftime("%B %Y")
+
+def _first_research_artefact_group():
+    """Return the first research artefact group."""
+    return _abbrdate() + 'a'
 
 class AcronymExtension(Extension):
     """Registers the `acronym` Jinja2 filter and global callable."""
@@ -16,5 +33,8 @@ class AcronymExtension(Extension):
         super().__init__(environment)
         # Register as both a filter  ({{ repo_name | acronym }})
         # and a global callable       ({{ acronym(repo_name) }})
+        environment.globals["abbrmonth"] = _abbrmonth
         environment.filters["acronym"] = _acronym
         environment.globals["acronym"] = _acronym
+        environment.globals["firstRAG"] = _first_research_artefact_group
+        environment.globals["month"] = _month
