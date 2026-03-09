a81150b645c0e71035ee98f55d475217a55d8606
Andreas Kempa-Liehr <kempa-liehr@protonmail.com>
2026-03-10 10:50:07 +1300

    Create project from template by direct call of cookiecutter().

--- src/contexere/conf.py
+++ src/contexere/conf.py
@@ -26,6 +26,7 @@ if not __CONTEXERE_CACHE_DIR__.exists():
     __CONTEXERE_CACHE_DIR__.mkdir()
 
 __CONTEXERE_CACHE_DB__ = __CONTEXERE_CACHE_DIR__ / 'context.db'
+__COOKIECUTTER_PATH__ = Path(__file__).parents[2]
 
 config_file_path = __CONTEXERE_CACHE_DIR__ / 'conf.ini'
 if not config_file_path.exists():
