dfd14e356e7ae71aaecdbedb6793311cfaf20577
Andreas W. Kempa-Liehr <a.kempa-liehr@auckland.ac.nz>
2026-04-10 09:34:46 +1200

    Introduce ResearchArtefactGroup class to provide a simple interface for querying RAG identifers

---
+++ src/contexere/data/groups.py
@@ -0,0 +1,40 @@
+"""
+Classes supporting the naming scheme.
+"""
+
+from contexere.data.context import confirm_rag
+from contexere.scheme import decode_abbreviated_datetime
+
+class ResearchArtefactGroup:
+    def __init__(self, identifier):
+        match, self.project, self.date, self.step, self.remainder = confirm_rag(identifier)
+        if not match:
+            raise ValueError(f"Reference '{identifier}' is not a research artefact group identifier.")
+        else:
+            self.identifier_ = identifier
+        self.iso_date = decode_abbreviated_datetime(self.date)
+        self.year = self.iso_date.year
+        self.month = self.iso_date.month
+        self.day = self.iso_date.day
+        self.__slots__ = ()
+
+    def __str__(self):
+        return self.identifier_
+
+    def common(self, other):
+        if type(other) == str:
+            other_group = ResearchArtefactGroup(other)
+        else:
+            other_group = other
+        common = ''
+        if self.project == other_group.project:
+            common += self.project
+            if self.year == other_group.year:
+                common += self.date[:2]
+                if self.month == other_group.month:
+                    common += self.date[2]
+                    if self.day == other_group.day:
+                        common += self.date[3]
+                        if self.step == other_group.step:
+                            common += self.step
+        return common
---
+++ tests/data/groups/test_ResearchArtefactGroups.py
@@ -0,0 +1,28 @@
+import pytest
+
+from contexere.data.groups import ResearchArtefactGroup
+
+def test_initialization():
+    group = ResearchArtefactGroup('ERP26rAa')
+    assert isinstance(group, ResearchArtefactGroup)
+    assert group.project == 'ERP'
+    assert group.date == '26rA'
+    assert group.step == 'a'
+
+    with pytest.raises(ValueError):
+        group = ResearchArtefactGroup('26rAa')
+
+def test_AttributeError():
+    group = ResearchArtefactGroup('ERP26rAa')
+
+    with pytest.raises(AttributeError):
+        print(group.no_attribute)
+
+def test_common():
+    group = ResearchArtefactGroup('ERP26rAa')
+
+    assert 'ERP26rAa' == group.common('ERP26rAa')
+    assert 'ERP26rA' == group.common('ERP26rAb')
+    assert 'ERP26r' == group.common('ERP26r9a')
+    assert 'ERP26' == group.common('ERP26q9a')
+    assert 'ERP' == group.common('ERP25q9a')
