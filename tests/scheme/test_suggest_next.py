kempa-liehr/contexere

commit 625743b9639704adacd0b5022619a7a65127c644

Always assume that the next identifier is requested.

src/contexere/scheme.py
@@ -81,8 +81,8 @@ def decode_abbreviated_datetime(abrv, tz=pytz.utc):
     return datetime.datetime(year, month, day, hour, minutes, tzinfo=tz)
 
 
-def suggest_next(directory='.', project=None, local=True):
-    context, timeline = build_context(directory, project_filter=project)
+def suggest_next(directory='.', project='', local=True, recursive=False):
+    context, timeline = build_context(directory, project_filter=project, recursive=recursive)
     logging.info('Projects' + str(list(context.keys())))
     logging.info('Timeline' + str(list(timeline.keys())))
     today = abbreviate_date(local=local)

src/contexere/nxt.py
@@ -38,18 +38,11 @@ def parse_args(args):
                         type=Path,
                         default=Path.cwd())
     parser.add_argument(
-        "-n",
-        "--next",
-        dest="next",
-        help="Suggest next artefact name",
-        action="store_true"
-    )
-    parser.add_argument(
         "-p",
         "--project",
         dest="project",
         type=str,
-        default=None,
+        default='',
         help="Specify project abbreviation",
         action="store"
     )
@@ -121,10 +114,8 @@ def main(args):
             print(summary(args.path, recursive=args.recursive))
         except ValueError as error:
             _logger.warning(error)
-    elif args.next:
-        print(suggest_next(args.path, project=args.project, local=~args.utc))
     else:
-        output = abbreviate_date(local=~args.utc)
+        output = suggest_next(args.path, project=args.project, local=~args.utc, recursive=args.recursive)
         if args.time:
             output += abbreviate_time(local=~args.utc)
         print(output)

commit 1dbef21763e0333284bb786f78471416596bf1a8

Fix error resulting from change of default project parameter.
The error was introduced in 625743b9.

src/contexere/scheme.py
@@ -86,7 +86,7 @@ def suggest_next(directory='.', project='', local=True, recursive=False):
     logging.info('Timeline' + str(list(timeline.keys())))
     today = abbreviate_date(local=local)
     if len(context) == 0:
-        if project is None:
+        if project == '':
             raise ValueError(f"No project files matching the naming scheme found in path {directory}"
                              "and option '--project' wasn't set.")
         else:
@@ -98,7 +98,7 @@ def suggest_next(directory='.', project='', local=True, recursive=False):
             match = schematic.match(latest[0])
         else:
             summary_df = summary(buffered_context=context)
-            if project is None:
+            if project == '':
                 summary_df.sort_values(by=['RAI'], ascending=False, inplace=True)
                 match = schematic.match(summary_df.index[0] + summary_df['Latest'].iloc[0])
                 logging.info("Continuing from latest RAI.")

tests/scheme/test_suggest_next.py
@@ -1,0 +1,41 @@
+import pytest
+
+from contexere.scheme import abbreviate_date, suggest_next
+
+@pytest.fixture()
+def temp_dir(tmp_path_factory):
+    # Create a named base directory for the session
+    base = tmp_path_factory.mktemp("session_data")
+    return base
+
+def fill_folder(path, *files):
+    for fn in files:
+        (path / fn).write_text("x", encoding="utf-8")
+
+def test_one_old_file(temp_dir):
+    fill_folder(temp_dir, 'notes.txt', 'ERP26pBa_example.txt')
+    next_rag = suggest_next(temp_dir, project='ERP')
+    assert next_rag == 'ERP' + abbreviate_date(local=True) +'a'
+
+def test_one_recent_file(temp_dir):
+    rag = abbreviate_date(local=True)
+    fill_folder(temp_dir, 'notes.txt', 'ERP' + rag + 'a_example.txt')
+    next_rag = suggest_next(temp_dir, project='ERP')
+    assert next_rag == 'ERP' + abbreviate_date(local=True) +'b'
+
+def test_no_previous_RAGs(temp_dir):
+    fill_folder(temp_dir, 'notes.txt')
+    next_rag = suggest_next(temp_dir, project='ERP')
+    assert next_rag == 'ERP' + abbreviate_date(local=True) +'a'
+
+def test_missing_project_error(temp_dir):
+    fill_folder(temp_dir, 'notes.txt')
+    with pytest.raises(ValueError):
+        suggest_next(temp_dir)
+
+def test_several_old_RAGs_and_no_project(temp_dir):
+    rag = abbreviate_date(local=True)
+    fill_folder(temp_dir, 'notes.txt', 'ERP26pBa_example.txt', 'ERP' + rag + 'a_example.txt',
+                'ERQ26pBa_example.txt')
+    next_rag = suggest_next(temp_dir)
+    assert next_rag == 'ERP' + abbreviate_date(local=True) +'b'