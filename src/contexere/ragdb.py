8b48149370430a3c0b49a20e8cb40565c2d26f5c
Andreas W. Kempa-Liehr <a.kempa-liehr@auckland.ac.nz>
2026-05-04 10:39:08 +1200

    Cloned from nxt.py.

---
+++ src/contexere/ragdb.py
@@ -0,0 +1,197 @@
+import argparse
+import logging
+from pathlib import Path
+import subprocess
+import sys
+
+
+from contexere import __version__
+from contexere.clone import clone_file
+from contexere.collect import summary
+from contexere.conf import __CONTEXERE_CACHE_DB__, __COOKIECUTTER_PATH__
+from contexere.data.cache import fill_cache
+from contexere.data.interfaces.contextdb import ContextDB
+from contexere.scheme import abbreviate_time, suggest_next
+
+__author__ = "Andreas W. Kempa-Liehr"
+__copyright__ = "Andreas W. Kempa-Liehr"
+__license__ = "MIT"
+
+_logger = logging.getLogger(__name__)
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--help"]``).
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(description="Suggest name for research artefact")
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"contexere {__version__}",
+    )
+    parser.add_argument(
+        "-i",
+        "--init-cache",
+        dest="init_cache",
+        help="Init context cache",
+        action="store_true"
+    )
+    parser.add_argument(dest="path",
+                        help="Path to folder with research artefacts (default: current working dir)",
+                        nargs='?',
+                        type=Path,
+                        default=Path.cwd())
+    parser.add_argument("-c_",
+                        "--clone",
+                        dest="clone",
+                        nargs=1,
+                        help="Clone file and commit cloned file to local repository.")
+    parser.add_argument("-d",
+                        "--database",
+                        dest="database",
+                        help=f"Path to SQLite database (default: {__CONTEXERE_CACHE_DB__})",
+                        type=Path,
+                        default=__CONTEXERE_CACHE_DB__)
+    parser.add_argument(
+        "-g",
+        "--group",
+        dest="group",
+        type=str,
+        default='',
+        help="Project identifier for which the next research artefact GROUP will be suggested",
+        action="store"
+    )
+    parser.add_argument("-k",
+                        "--keywords",
+                        nargs="+",
+                        dest="keywords",
+                        help="Optional argument for --clone adding one or more keywords to the filename",
+                        )
+    parser.add_argument(
+        "-l",
+        "--local",
+        dest="local",
+        help="Inspect files in current working dir only",
+        action="store_true"
+    )
+    parser.add_argument("-p",
+                        "--project",
+                        dest="project",
+                        help="Create new project directory structure",
+                        action="store_true")
+    parser.add_argument("-r",
+                        "--reference",
+                        nargs="+",
+                        dest="reference",
+                        help="Optional argument for --clone referencing one or more search artefact groups",
+                        )
+    parser.add_argument(
+        "-s",
+        "--summary",
+        dest="summary",
+        help="Summarise files following the naming convention",
+        action="store_true"
+    )
+    parser.add_argument(
+        "-t",
+        "--time",
+        dest="time",
+        help="add time abbreviation",
+        action="store_true"
+    )  
+    parser.add_argument(
+        "-u",
+        "--utc",
+        dest="utc",
+        help="Generate timestamp with respect to UTC (default is local timezone)",
+        action="store_true"
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG,
+    )
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+def main(args):
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    _logger.debug("Start building context...")
+    if args.init_cache:
+        db = ContextDB(path=args.database)
+        fill_cache(db, root=args.path)
+    elif args.summary:
+        try:
+            print(summary(args.path, recursive=~args.local))
+        except ValueError as error:
+            _logger.warning(error)
+    elif args.project:
+        subprocess.call(["ccds", "--output-dir", args.path, str(__COOKIECUTTER_PATH__)])
+    else:
+        next_rag = suggest_next(args.path, project=args.group, local=~args.utc, recursive=~args.local)
+        print(args.path, args.clone)
+        if args.clone:
+            path = clone_file(args.path / args.clone[0], next_rag,
+                              reference=args.reference,
+                              keywords=args.keywords)
+            print(f"Clone {path} from {args.clone}.")
+        else:
+            if args.time:
+                output += abbreviate_time(local=~args.utc)
+            print(output)
+
+    # print(args.project + abbreviate_date() + ending)
+    _logger.info("Script ends here")
+
+
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    # ^  This is a guard statement that will prevent the following code from
+    #    being executed in the case someone imports this file instead of
+    #    executing it as a script.
+    #    https://docs.python.org/3/library/__main__.html
+
+    # After installing your project with pip, users can also run your Python
+    # modules as scripts via the ``-m`` flag, as defined in PEP 338::
+    #
+    #     python -m contexere.name
+    #
+    run()
