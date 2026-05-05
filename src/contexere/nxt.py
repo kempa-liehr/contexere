kempa-liehr/contexere

commit c7c4c5d3d6505533838e73627c447ea57f966b15

Sensible interpretation of CLI argument

src/contexere/nxt.py
@@ -4,11 +4,11 @@ from pathlib import Path
 import subprocess
 import sys
 
-
 from contexere import __version__
 from contexere.clone import clone_file
 from contexere.collect import summary
 from contexere.conf import __CONTEXERE_CACHE_DB__, __COOKIECUTTER_PATH__
+from contexere.data.context import confirm_partial_rag, confirm_project_identifier, confirm_rag
 from contexere.data.cache import fill_cache
 from contexere.data.interfaces.contextdb import ContextDB
 from contexere.scheme import abbreviate_time, suggest_next
@@ -36,16 +36,6 @@ def parse_args(args):
         action="version",
         version=f"contexere {__version__}",
     )
-    parser.add_argument(dest="path",
-                        help="Path to folder with research artefacts (default: current working dir)",
-                        nargs='?',
-                        type=Path,
-                        default=Path.cwd())
-    parser.add_argument("-c_",
-                        "--clone",
-                        dest="clone",
-                        nargs=1,
-                        help="Clone file and commit cloned file to local repository.")
     parser.add_argument(
         "-g",
         "--group",
@@ -116,6 +106,11 @@ def parse_args(args):
         action="store_const",
         const=logging.DEBUG,
     )
+    parser.add_argument(
+        "target",
+        nargs="?",
+        help="Either a project identifier, filename, or folder"
+    )
     return parser.parse_args(args)
 
 
@@ -130,32 +125,56 @@ def setup_logging(loglevel):
         level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
     )
 
+def process_nxt(args):
+    """
+    Process the command line arguments while choosing the most sensible action.
+    """
+    cloned = False
+    if args.target is None:
+        group = args.group if args.group != '' else summary(Path.cwd(), recursive=~args.local).index[0]
+        output = suggest_next(Path.cwd(), project=group, local=~args.utc, recursive=~args.local)
+    else:
+        path = Path.cwd() / args.target
+        if path.is_dir():
+            group = args.group if args.group != '' else summary(path, recursive=~args.local).index[0]
+            output = suggest_next(path, project=group, local=~args.utc, recursive=~args.local)
+        elif path.exists():
+            match, project, date, step, remainder = confirm_rag(path.stem)
+            if match:
+                next_rag = suggest_next(path.parents[0], project=project, local=~args.utc, recursive=(not args.local))
+                output, message = clone_file(path, next_rag, reference=args.reference, keywords=args.keywords)
+                cloned = True
+            else:
+                fn = path.name
+                raise ValueError(f'Filename `{fn}` does not start with a research artefact group identifier.')
+        else:
+            match, project = confirm_project_identifier(args.target)
+            if match:
+                output = suggest_next(Path.cwd(), project=project, local=~args.utc, recursive=~args.local)
+            else:
+                raise ValueError(f"`The argument `{args.target}` is neither a project identifier nor a filename.")
+
+    if args.time and not cloned:
+        output += abbreviate_time(local=~args.utc)
+    return output
+
+    return output
+
 
 def main(args):
     args = parse_args(args)
     setup_logging(args.loglevel)
-    _logger.debug("Start building context...")
-    if args.summary:
+    if args.project:
+        subprocess.call(["ccds", "--output-dir", args.path, str(__COOKIECUTTER_PATH__)])
+    elif args.summary:
+        _logger.debug("Start building context ...")
         try:
             print(summary(args.path, recursive=~args.local))
         except ValueError as error:
             _logger.warning(error)
-    elif args.project:
-        subprocess.call(["ccds", "--output-dir", args.path, str(__COOKIECUTTER_PATH__)])
     else:
-        next_rag = suggest_next(args.path, project=args.group, local=~args.utc, recursive=~args.local)
-        print(args.path, args.clone)
-        if args.clone:
-            path = clone_file(args.path / args.clone[0], next_rag,
-                              reference=args.reference,
-                              keywords=args.keywords)
-            print(f"Clone {path} from {args.clone}.")
-        else:
-            if args.time:
-                output += abbreviate_time(local=~args.utc)
-            print(output)
-
-    # print(args.project + abbreviate_date() + ending)
+        output = process_nxt(args)
+        print(output)
     _logger.info("Script ends here")
 
 

src/contexere/collect.py
@@ -32,7 +32,7 @@ def build_context(directory='.', project_filter='', recursive=False):
         file_iterator = Path(directory).glob(project_filter + '*')
     # Iterate over files and directories in the specified folder
     for path in file_iterator:
-        match = pattern.match(path.name)
+        match = pattern.match(path.stem)
         if match:
             project = match.group('project')
             date = match.group('date')

src/contexere/data/context.py
@@ -12,6 +12,7 @@ __pattern__ = re.compile(r'^(?P<project>[a-zA-Z]{2,})'
 __partial__ = re.compile(r'^(?:(?P<project>[a-zA-Z]{2,})(?=\d))?'
                          r'(?P<date>\d{2}[o-z][1-9A-V]|[o-z][1-9A-V]|[1-9A-V]|)'
                          r'(?P<step>[a-z])$')
+__project__ = re.compile(r'^(?P<project>[a-zA-Z]{2,})')
 
 def confirm_rag(token, pattern=__pattern__):
     match = pattern.match(token)
@@ -33,6 +34,14 @@ def confirm_partial_rag(token, pattern=__partial__):
     else:
         project, date, step = None, None, None
     return match, project, date, step
+
+def confirm_project_identifier(token, pattern=__project__):
+    match = pattern.match(token)
+    if match:
+        project = match.group('project')
+    else:
+        project = None
+    return match, project
 
 def index_file_artefact(db, filepath, project, date, step, remainder):
     project_id = db.upsert('Project', dict(Name=project))

tests/data/context/test_confirm_project_identifier.py
@@ -1,0 +1,15 @@
+from contexere.data.context import confirm_project_identifier
+
+def test_confirm_project_identifier():
+    match, project = confirm_project_identifier('ERP')
+    assert match is not None
+    assert project == 'ERP'
+
+    match, project = confirm_project_identifier('erp')
+    assert match is not None
+    assert project == 'erp'
+
+def test_confirm_project_identifier_failed_too_short():
+    match, project = confirm_project_identifier('E')
+    assert match is None
+    assert project is None

commit 13ec825d19549af8200f703db2a8312c75923d32

Fix error introduced in c7c4c5d3d6505533838e73627c447ea57f966b15

src/contexere/nxt.py
@@ -185,12 +185,13 @@ def process_nxt(args):
 def main(args):
     args = parse_args(args)
     setup_logging(args.loglevel)
+    path = Path.cwd() if args.target is None else args.target
     if args.project:
-        subprocess.call(["ccds", "--output-dir", args.path, str(__COOKIECUTTER_PATH__)])
+        subprocess.call(["ccds", "--output-dir", path, str(__COOKIECUTTER_PATH__)])
     elif args.summary:
         _logger.debug("Start building context ...")
         try:
-            print(summary(args.path, recursive=~args.local))
+            print(summary(path, recursive=~args.local))
         except ValueError as error:
             _logger.warning(error)
     else: