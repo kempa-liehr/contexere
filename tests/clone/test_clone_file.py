2623b8177a54f53f799be66cc743ba1050be9734
Andreas W. Kempa-Liehr <a.kempa-liehr@auckland.ac.nz>
2026-05-05 17:38:33 +1200

    Test cloning of files without RAG.

--- tests/clone/test_clone_file.py
+++ tests/clone/test_clone_file.py
@@ -19,6 +19,18 @@ def test_one_old_file_no_reference(temp_dir):
     path_to_clone, message = clone_file(temp_dir / 'ERP25pBa__example.txt', rag)
     assert (temp_dir / (rag + '__example.txt')).exists()
 
+def test_file_without_rag(temp_dir):
+    fill_folder(temp_dir, 'notes.txt', 'ERP25pBa__example.txt')
+    rag = 'ERP' + abbreviate_date(local=True) + 'a'
+    path_to_clone, message = clone_file(temp_dir / 'notes.txt', rag)
+    assert (temp_dir / (rag + '.txt')).exists()
+
+def test_file_without_rag_with_notes(temp_dir):
+    fill_folder(temp_dir, 'notes_example.txt', 'ERP25pBa__example.txt')
+    rag = 'ERP' + abbreviate_date(local=True) + 'a'
+    path_to_clone, message = clone_file(temp_dir / 'notes_example.txt', rag)
+    assert (temp_dir / (rag + '.txt')).exists()
+
 def test_one_old_file_with_reference(temp_dir):
     fill_folder(temp_dir, 'notes.txt', 'ERP25pBa__example.txt')
     rag = 'ERP' + abbreviate_date(local=True) + 'a'
