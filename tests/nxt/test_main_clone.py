570be5a4dbe420376199ebaad2a45417388bc0ec
Andreas W. Kempa-Liehr <a.kempa-liehr@auckland.ac.nz>
2026-05-06 20:45:37 +1200

    Test cloning if references have whitespaces.

--- tests/nxt/test_main_clone.py
+++ tests/nxt/test_main_clone.py
@@ -28,6 +28,15 @@ def test_clone(temp_dir):
     print(list(temp_dir.glob('*')))
     assert (temp_dir / (rag + '__example.txt')).exists()
 
+def test_clone_with_reference_having_whitespace(temp_dir):
+    fill_folder(temp_dir, 'notes.txt', 'ERP25pBa__example.txt')
+    os.chdir(temp_dir)
+    rag = 'ERP' + abbreviate_date(local=True) + 'a'
+
+    main(['ERP25pBa__example.txt', '--keywords', 'test A'])
+    print(list(temp_dir.glob('*')))
+    assert (temp_dir / (rag + '__test_A.txt')).exists()
+
 def test_clone_with_own_reference(temp_dir):
     fill_folder(temp_dir, 'notes.txt', 'ERP25pBa_Aa__example.txt')
     os.chdir(temp_dir)
